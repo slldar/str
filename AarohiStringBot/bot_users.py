9eb2a1c7eb9e0349b8011dda56783be89e436650
slldar <165353541+slldar@users.noreply.github.com>
2024-04-15 17:52:56 +0200

    Update bot_users.py

--- AarohiStringBot/bot_users.py
+++ AarohiStringBot/bot_users.py
@@ -10,7 +10,7 @@ async def users_sql(_, msg: Message):
     await add_served_user(msg.from_user.id)
 
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command("الاحصائيات", prefixes=["/", ""]))
 async def _stats(_, msg: Message):
     users = len(await get_served_users())
     await msg.reply_text(f"𓏺 𝖼𝖴𝗋𝗋𝖤𝗇𝖳 𝗌𝖳𝖺𝖳𝗌 𝖮𝖥 𝗌𝖳𝖱𝗂𝗇𝖦 𝖦𝖾𝖭 𝖡𝗈𝖳 :\n\n {users}  𝖴𝗌𝖤𝖱𝗌 .", quote=True)
