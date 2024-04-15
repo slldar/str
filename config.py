7f396866174b7f3edce428d11cb7f13e9d372965
slldar <165353541+slldar@users.noreply.github.com>
2024-04-15 20:28:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","14050586"))
-API_HASH = getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
+API_ID = int(getenv("API_ID","15575514"))
+API_HASH = getenv("API_HASH","0152346497bdf77eb9698994d0205a3a")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://SlliDaaR:SlliDaaR@cluster0.rwmk3kf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN", None)
