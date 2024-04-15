9676661635c10213eb5b4a1803f72c10259965a2
slldar <165353541+slldar@users.noreply.github.com>
2024-04-15 21:33:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = getenv("API_HASH","0152346497bdf77eb9698994d0205a3a")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://SlliDaaR:SlliDaaR@cluster0.rwmk3kf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://SlliDaaR:SlliDaaR@cluster0.rwmk3kf.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", None)
